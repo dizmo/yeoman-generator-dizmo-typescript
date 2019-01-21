617ec743ec54b1e71b627db4efa894cb9e350f3c
Hasan Karahan <hasan.karahan@blackhan.com>
2019-01-21 18:47:32 +0300

    DIG-220: correctly incl. @babel/polyfill

--- templates/gulp/tasks/build/scripts/index.js
+++ templates/gulp/tasks/build/scripts/index.js
@@ -96,7 +96,7 @@ gulp.task('scripts', function () {
 
     let browserified = browserify({
         basedir: '.', debug: !!argv.sourcemaps, entries: [
-            'node_modules/@babel/polyfill/lib/index.js', 'src/app/app.ts'
+            'node_modules/@babel/polyfill/dist/polyfill.js', 'src/app/app.ts'
         ]
     }).plugin(tsify).transform('babelify', {
         presets: ['@babel/preset-env'], extensions: ['.ts']
