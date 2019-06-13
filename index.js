3750de3156cef97780a8936de4b783a13b5e90e0
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-13 18:32:50 +0300

    DIG-242: refactored index.js

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 'use strict';
 
-let fs = require('fs'),
-    Generator = require('yeoman-generator'),
-    lodash = require('lodash'),
-    rimraf = require('rimraf');
-
-function sort(dictionary) {
-    let array = [],
-        sorted = {};
-
-    for(let key in dictionary) {
-        array[array.length] = key;
-    }
-    array.sort();
-
-    for(let i = 0; i < array.length; i++) {
-        sorted[array[i]] = dictionary[array[i]];
-    }
-    return sorted;
-}
+const fs = require('fs');
+const Generator = require('yeoman-generator');
+const lodash = require('lodash');
+const rimraf = require('rimraf');
 
 module.exports = class extends Generator {
     writing() {
-        let upgrade = Boolean(
-            this.options.upgrade && fs.existsSync('package.json'));
+        const upgrade = Boolean(
+            this.options.upgrade && fs.existsSync('package.json')
+        );
         if (!upgrade || upgrade) {
             this.fs.copy(
                 this.templatePath('gulp/'),
-                this.destinationPath('gulp/'));
+                this.destinationPath('gulp/')
+            );
         }
         if (!upgrade || upgrade) {
-            let pkg = this.fs.readJSON(
+            const pkg = this.fs.readJSON(
                 this.destinationPath('package.json')
             );
             pkg.dependencies = sort(
@@ -49,32 +36,42 @@ module.exports = class extends Generator {
                     'gulp-tslint': '^8.1.4',
                     'tsify': '^4.0.1',
                     'tslint': '^5.17.0',
-                    'typescript': '^3.5.1'
+                    'typescript': '^3.5.2'
                 })
             );
             delete pkg.devDependencies['gulp-eslint'];
             delete pkg['babel'];
             this.fs.writeJSON(
-                this.destinationPath('package.json'), pkg, null, 2);
+                this.destinationPath('package.json'), pkg, null, 2
+            );
         }
         if (!upgrade) {
             this.fs.copy(
                 this.templatePath('src/'),
-                this.destinationPath('src/'));
+                this.destinationPath('src/')
+            );
             this.fs.copy(
                 this.templatePath('tslint.json'),
-                this.destinationPath('tslint.json'));
+                this.destinationPath('tslint.json')
+            );
             this.fs.copy(
                 this.templatePath('tsconfig.json'),
-                this.destinationPath('tsconfig.json'));
+                this.destinationPath('tsconfig.json')
+            );
         }
         this.conflicter.force = this.options.force || upgrade;
     }
-
     end() {
         rimraf.sync(
-            this.destinationPath('.eslintrc.json'));
+            this.destinationPath('.eslintrc.json')
+        );
         rimraf.sync(
-            this.destinationPath('src/index.js'));
+            this.destinationPath('src/index.js')
+        );
     }
 };
+function sort(object) {
+    return Object.entries(object).sort().reduce(
+        (a, [k, v]) => { a[k] = v; return a; }, {}
+    );
+}
